02aeb04a73cf8c0b7fdc86aca2ecebc3a34d4390
Austin Hartline <austinhartline@gmail.com>
2017-02-13 22:44:39 -0600

    laptop stuff

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/env zsh
 
-ln -sv xinitrc $HOME/.xinitrc
-ln -sv yaourtrc $HOME/.yaourtrc
-ln -sv zshrc $HOME/.zshrc
-mkdir -p $HOME/.config/i3 && ln -sv config/i3/config $HOME/.config/i3/config
-mkdir -p $HOME/.config/i3status && ln -sv config/i3status/config $HOME/.config/i3status/config
-mkdir -p $HOME/.config/remmina && ln -sv config/remmina/remmina.pref $HOME/.config/remmina/remmina.pref
-mkdir -p $HOME/.config/termite && ln -sv config/termite/config $HOME/.config/termite/config
+pwd=${PWD}
+
+ln -sv $pwd/xinitrc $HOME/.xinitrc
+ln -sv $pwd/yaourtrc $HOME/.yaourtrc
+ln -sv $pwd/zshrc $HOME/.zshrc
+mkdir -p $HOME/.config/i3 && ln -sv $pwd/config/i3/config $HOME/.config/i3/config
+mkdir -p $HOME/.config/i3status && ln -sv $pwd/config/i3status/config $HOME/.config/i3status/config
+mkdir -p $HOME/.config/remmina && ln -sv $pwd/config/remmina/remmina.pref $HOME/.config/remmina/remmina.pref
+mkdir -p $HOME/.config/termite && ln -sv $pwd/config/termite/config $HOME/.config/termite/config
 
